506596f9abb2bb78e819ac0c569a1c39a1b1c3be
azhar007elhar <azhar7elhar@gmail.com>
2020-12-18 22:22:09 +0100

    commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,140 +8,163 @@ import { IonSlides } from '@ionic/angular';
 })
 export class Tab1Page {
 
+
+
+
   @ViewChild('slideWithNav', { static: false }) slideWithNav: IonSlides;
-    @ViewChild('slideWithNav2', { static: false }) slideWithNav2: IonSlides;
-    @ViewChild('slideWithNav3', { static: false }) slideWithNav3: IonSlides;
-  
-    sliderOne: any;
-    sliderTwo: any;
-    sliderThree: any;
-  
-  
-    //Configuration for each Slider
-    slideOptsOne = {
-      initialSlide: 0,
-      slidesPerView: 1,
-      autoplay: true
-    };
-    slideOptsTwo = {
-      initialSlide: 1,
-      slidesPerView: 2,
-      loop: true,
-      centeredSlides: true,
-      spaceBetween: 20
-    };
-    slideOptsThree = {
-      initialSlide: 0,
-      slidesPerView: 3
-    };
-  
-    constructor(
-    ) {
-      //Item object for Nature
-      this.sliderOne =
-      {
-        isBeginningSlide: true,
-        isEndSlide: false,
-        slidesItems: [
-          {
-            id: 995
-          },
-          {
-            id: 925
-          },
-          {
-            id: 940
-          },
-          {
-            id: 943
-          },
-          {
-            id: 944
-          }
-        ]
-      };
-      //Item object for Food
-      this.sliderTwo =
-      {
-        isBeginningSlide: true,
-        isEndSlide: false,
-        slidesItems: [
-          {
-            id: 324
-          },
-          {
-            id: 321
-          },
-          {
-            id: 435
-          },
-          {
-            id: 524
-          },
-          {
-            id: 235
-          }
-        ]
-      };
-      //Item object for Fashion
-      this.sliderThree =
-      {
-        isBeginningSlide: true,
-        isEndSlide: false,
-        slidesItems: [
-          {
-            id: 643
-          },
-          {
-            id: 532
-          },
-          {
-            id: 232
-          },
-          {
-            id: 874
-          },
-          {
-            id: 193
-          }
-        ]
-      };
+  @ViewChild('slideWithNav2', { static: false }) slideWithNav2: IonSlides;
+  @ViewChild('slideWithNav3', { static: false }) slideWithNav3: IonSlides;
+
+  sliderOne: any;
+  sliderTwo: any;
+  sliderThree: any;
+  recipes: any;
+  recipesCategory: any;
+  recipesLatest: any;
+
+  //Configuration for each Slider
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true
+  };
+  slideOptsTwo = {
+    initialSlide: 1,
+    slidesPerView: 2,
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 20
+  };
+  slideOptsThree = {
+    initialSlide: 0,
+    slidesPerView: 3
+  };
+
+  constructor(
+  ) {
+
+    this.recipesLatest = {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      items: [
+        {
+          name: 'Chicken White Rice',
+          image: 'assets/img/R1.jpg'
+        },
+        {
+          name: 'Cocoa Sponge Cake',
+          image: 'assets/img/C2.png'
+        },
+      ]
     }
-  
-    //Move to Next slide
-    slideNext(object, slideView) {
-      slideView.slideNext(500).then(() => {
-        this.checkIfNavDisabled(object, slideView);
-      });
+
+    this.recipesCategory = {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      items: [
+        {
+          name: 'Cakes & Pancakes',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name: 'Sweets & Halwa',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name: 'Chinese & Italian',
+          image: 'assets/img/C3.jpg'
+        },
+        {
+          name: 'Kebab & Parathas',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name: 'Sea food',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name: 'Drinks',
+          image: 'assets/img/C1.jpg'
+        }
+      ]
     }
-  
-    //Move to previous slide
-    slidePrev(object, slideView) {
-      slideView.slidePrev(500).then(() => {
-        this.checkIfNavDisabled(object, slideView);
-      });;
+
+    this.recipes = {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      items: [
+        {
+          name: 'Barbeque Sandwiches',
+          time: 25,
+          image: 'assets/img/R1.jpg',
+          category: 'Food'
+        },
+        {
+          name: 'Barbeque Sandwiches',
+          time: 25,
+          image: 'assets/img/R1.jpg',
+          category: 'Food'
+        }
+        ,
+        {
+          name: 'Barbeque Sandwiches',
+          time: 25,
+          image: 'assets/img/R1.jpg',
+          category: 'Food'
+        }
+        ,
+        {
+          name: 'Barbeque Sandwiches',
+          time: 25,
+          image: 'assets/img/R1.jpg',
+          category: 'Food'
+        }
+
+      ]
     }
-  
-    //Method called when slide is changed by drag or navigation
-    SlideDidChange(object, slideView) {
-      this.checkIfNavDisabled(object, slideView);
-    }
-  
-    //Call methods to check if slide is first or last to enable disbale navigation  
-    checkIfNavDisabled(object, slideView) {
-      this.checkisBeginning(object, slideView);
-      this.checkisEnd(object, slideView);
-    }
-  
-    checkisBeginning(object, slideView) {
-      slideView.isBeginning().then((istrue) => {
-        object.isBeginningSlide = istrue;
-      });
-    }
-    checkisEnd(object, slideView) {
-      slideView.isEnd().then((istrue) => {
-        object.isEndSlide = istrue;
-      });
-    }
-  
+
   }
+
+
+  toggleDarkMode = () => {    
+    document.body.classList.toggle('dark');
+  }
+
+
+  //Move to Next slide
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
+  }
+
+  //Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });;
+  }
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
+    this.checkIfNavDisabled(object, slideView);
+  }
+
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
+
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+    });
+  }
+  checkisEnd(object, slideView) {
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+    });
+  }
+
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  recipesCategory: any ; 
+
+  constructor() {
+
+    this.recipesCategory = {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      items: [
+        {
+          name : 'Cakes & Pancakes' ,
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name : 'Sweets & Halwa',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name : 'Chinese & Italian',
+          image: 'assets/img/C3.jpg'
+        },
+        {
+          name : 'Kebab & Parathas',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name : 'Sea food',
+          image: 'assets/img/C1.jpg'
+        },
+        {
+          name : 'Drinks',
+          image: 'assets/img/C1.jpg'
+        }
+      ]
+     }
+
+
+  }
 
 }
